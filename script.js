501c2bedefcf3220937c58b328fa5800e990ca06
jheise <julianheise@gmx.net>
2012-10-25 13:35:49 +0200

    reworked javascript to closure style ready function

--- script.js
+++ script.js
@@ -1,62 +1,64 @@
-var COOKIE_DESCRIPTION_NAME = 'DW_Admin_ConfManager_showDescription';
-
-var showDescription = true;
-
 jQuery(document).ready(function() {
+	
+	var COOKIE_DESCRIPTION_NAME = 'DW_Admin_ConfManager_showDescription';
+	
+	var readCookie = function(cookieKey) {
+		var ARRcookies=document.cookie.split(";");
+		for (var i=0;i<ARRcookies.length;i++) {
+			var key = ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+			var value = ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+			key = key.replace(/^\s+|\s+$/g,"");
+			if (key == cookieKey) {
+				return unescape(value);
+			}
+		}
+	};
+	
+	var setCookie = function(key, value) {
+		var expirationDate = new Date();
+		expirationDate.setDate(expirationDate.getDate()+365);
+		document.cookie = escape(key) + '=' + escape(value) + '; expires=' + expirationDate.toUTCString();
+	};
+	
 	var cookie = readCookie(COOKIE_DESCRIPTION_NAME);
 	if(cookie == null) {
 		cookie = true;
 	}
-	showDescription = (cookie == 'false' ? false : true);
+	
+	var showDescription = (cookie == 'false' ? false : true);
 	if(!showDescription) {
 		jQuery('#description').hide();
 		jQuery('#description_toggle_button').attr('src', 'lib/plugins/confmanager/icons/expand.png');
 		return;
 	}
+	
 	jQuery('#description_toggle_button').attr('src', 'lib/plugins/confmanager/icons/collapse.png');
-});
-
-function toggleDescription() {
-	if(!showDescription) {
-		jQuery('#description').show();
-		jQuery('#description_toggle_button').attr('src', 'lib/plugins/confmanager/icons/collapse.png');
-	} else {
-		jQuery('#description').hide();
-		jQuery('#description_toggle_button').attr('src', 'lib/plugins/confmanager/icons/expand.png');
-	}
-	showDescription = !showDescription;
-	setCookie(COOKIE_DESCRIPTION_NAME, showDescription);
-}
-
-function setCookie(key, value) {
-	var expirationDate = new Date();
-	expirationDate.setDate(expirationDate.getDate()+365);
-	document.cookie = escape(key) + '=' + escape(value) + '; expires=' + expirationDate.toUTCString();
-}
-
-function readCookie(cookieKey) {
-	var ARRcookies=document.cookie.split(";");
-	for (var i=0;i<ARRcookies.length;i++) {
-		var key = ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-		var value = ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-		key = key.replace(/^\s+|\s+$/g,"");
-		if (key == cookieKey) {
-			return unescape(value);
+	
+	jQuery('#toggleDescription').click(function() {
+		if(!showDescription) {
+			jQuery('#description').show();
+			jQuery('#description_toggle_button').attr('src', 'lib/plugins/confmanager/icons/collapse.png');
+		} else {
+			jQuery('#description').hide();
+			jQuery('#description_toggle_button').attr('src', 'lib/plugins/confmanager/icons/expand.png');
 		}
-	}
-}
+		showDescription = !showDescription;
+		setCookie(COOKIE_DESCRIPTION_NAME, showDescription);
+		return false;
+	});
+	
+	var submitForm = function(name) {
+		document.forms[name].submit();
+	};
+	
+	var deleteLine = function(nr) {
+		jQuery('#value'+nr).val('');
+		jQuery('#tableLine'+nr).toggle();
+	};
 
-function submitForm(name) {
-	document.forms[name].submit();
-}
-
-function deleteLine(nr) {
-	jQuery('#value'+nr).val('');
-	jQuery('#tableLine'+nr).toggle();
-}
-
-function renameLine(nr) {
-	var key = jQuery('#key'+nr).text();
-	jQuery('#value'+nr).val('');
-	jQuery('#tableLine'+nr).toggle();
-}
+	var renameLine = function(nr) {
+		var key = jQuery('#key'+nr).text();
+		jQuery('#value'+nr).val('');
+		jQuery('#tableLine'+nr).toggle();
+	};
+});
--- tpl/descriptionHeader.php
+++ tpl/descriptionHeader.php
@@ -1,7 +1,7 @@
 <?php $helper = plugin_load('helper', 'confmanager'); ?>
 <h2><?php echo $configHeadLine ?>
 	<span class="heading_appendix">
-		<a href="javascript:toggleDescription()">
+		<a id="toggleDescription" href="">
 			<img id="description_toggle_button" title="<?php echo $helper->getLang('toggle_description') ?>" />
 		</a>
 	</span>
