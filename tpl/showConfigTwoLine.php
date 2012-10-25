626c9288f3727eddae2e0474b4a715133e69a5ab
jheise <julianheise@gmx.net>
2012-10-25 13:11:23 +0200

    splitted default and local values in two line table view; now two independent tables

--- tpl/showConfigTwoLine.php
+++ tpl/showConfigTwoLine.php
@@ -1,39 +1,21 @@
 <?php $helper = plugin_load('helper', 'confmanager'); ?>
-<?php $lineCounter = 0; ?>
 <div class="table">
+	<h3>User defined values</h3>
     <table class="inline confmanager_twoLine">
         <tr>
             <th><?php echo $helper->getLang('key') ?></th>
             <th><?php echo $helper->getLang('value') ?></th>
             <th><?php echo $helper->getLang('actions') ?></th>
         </tr>
-        <?php foreach ($configs as $key => $value): ?>
-            <?php
-            $defaultValue = false;
-            if (array_key_exists($key, $default)) {
-                if ($default[$key] === $value) {
-                    $defaultValue = true;
-                }
-            }
-            ?>
+        <?php $lineCounter = 0; ?>
+        <?php foreach ($local as $key => $value): ?>
             <tr id="tableLine<?php echo $lineCounter ?>">
                 <td>
-                <?php if ($defaultValue): ?>
-                	<div class="defaultValue" title="<?php echo hsc($helper->getLang('default_value_tooltip')) ?>">
-                        <?php echo hsc($key) ?>
-                    </div>
-                <?php else: ?>
                 	<label for="value<?php echo $lineCounter ?>" id="key<?php echo $lineCounter ?>">
                         <?php echo hsc($key) ?>
                     </label>
-                <?php endif ?>
                 </td>
                 <td>
-                <?php if ($defaultValue): ?>
-                	<div class="defaultValue" title="<?php echo hsc($helper->getLang('default_value_tooltip')) ?>">
-                        <?php echo hsc($value) ?>
-                    </div>
-                <?php else: ?>
                     <input
                     	id="value<?php echo $lineCounter ?>"
                         type="text"
@@ -41,17 +23,8 @@
                         value="<?php echo hsc($value) ?>"
                         class="edit"
                         />
-                <?php endif ?>
                 </td>
                 <td>
-                	<?php if ($defaultValue): ?>
-                	<img src="<?php echo 'lib/plugins/confmanager/icons/delete_disabled.png' ?>"
-                        	alt="<?php echo hsc($helper->getLang('delete_action')) ?>"
-                        	title="<?php echo hsc($helper->getLang('delete_action_tooltip_disabled')) ?>" />
-                    <img src="<?php echo 'lib/plugins/confmanager/icons/textfield_key_disabled.png' ?>"
-                        	alt="<?php echo hsc($helper->getLang('delete_action')) ?>"
-                        	title="<?php echo hsc($helper->getLang('edit_key_action_tooltip_disabled')) ?>" />
-                    <?php else: ?>
                     <a onclick="deleteLine(<?php echo $lineCounter ?>)">
 	                    <img src="<?php echo 'lib/plugins/confmanager/icons/delete.png' ?>"
 	                        	alt="<?php echo hsc($helper->getLang('delete_action')) ?>"
@@ -62,7 +35,6 @@
 	                        	alt="<?php echo hsc($helper->getLang('edit_key_action')) ?>"
 	                        	title="<?php echo hsc($helper->getLang('edit_key_action_tooltip')) ?>" />
                     </a>
-                    <?php endif ?>
                 </td>
             </tr>
             <?php $lineCounter++ ?>
@@ -76,4 +48,21 @@
             </td>
         </tr>
     </table>
+    <h3>Default values</h3>
+	<table class="inline confmanager_twoLine">
+		<tr>
+			<th><?php echo $helper->getLang('key') ?></th>
+            <th><?php echo $helper->getLang('value') ?></th>
+		</tr>
+		<?php foreach($default as $key => $value): ?>
+			<tr>
+				<td class="defaultValue">
+					<?php echo hsc($key); ?>
+				</td>
+				<td class="defaultValue">
+					<?php echo hsc($value); ?>
+				</td>
+			</tr>
+		<?php endforeach ?>
+	</table>
 </div>
