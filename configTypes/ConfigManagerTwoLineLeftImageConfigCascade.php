c8b6e6536b3690c2d92cfdeec1034e201d3ff7ce
Dominik Eckelmann <eckelmann@cosmocode.de>
2012-12-03 10:29:43 +0100

    added debug statements

--- configTypes/ConfigManagerTwoLineLeftImageConfigCascade.php
+++ configTypes/ConfigManagerTwoLineLeftImageConfigCascade.php
@@ -39,32 +39,39 @@ class ConfigManagerTwoLineLeftImageConfigCascade extends ConfigManagerTwoLineCas
     public function upload() {
         global $INPUT;
         if (!isset($_FILES['icon'])) {
+            dbglog('no icon send', 'confmanager upload');
             return false;
         }
         $icon = $_FILES['icon'];
         $key = $INPUT->str('key');
         if ($key === '') {
+            dbglog('no key send', 'confmanager upload');
             return false;
         }
         $configs = $this->readConfig();
         if (in_array($key, $configs['local'])) {
+            dbglog('cannot overwrite default key', 'confmanager upload');
             return false;
         }
 
         if ($icon['error'] != UPLOAD_ERR_OK) {
+            dbglog('some upload error ' . $icon['error'], 'confmanager upload');
             return false;
         }
 
         $extension = strrpos($icon['name'], '.');
         if ($extension === false) {
+            dbglog('no extension', 'confmanager upload');
             return false;
         }
         $extension = substr($icon['name'], $extension+1);
         if ($extension !== $this->extension) {
+            dbglog('wrong extension', 'confmanager upload');
             return false;
         }
 
         if (!move_uploaded_file($icon['tmp_name'], DOKU_INC . $this->imageFolder . "$key." . $this->extension)) {
+            dbglog('cannot move it to according folder', 'confmanager upload');
             return false;
         }
 
